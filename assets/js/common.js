8f058bbce2bd8f8d44fbd7c20ae86ee95dbc9b7a
Tanvi Desktop <javed.mistari@webmaffia.com>
2023-05-01 23:31:59 +0530

    home page

--- assets/js/common.js
+++ assets/js/common.js
@@ -2,30 +2,18 @@
     'use strict';
 
     // write code here
-    const $body = $('body');
+    $('.slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        infinite: true,
+        arrows: false,
+        dots: true,
 
-    let oldScroll = 0;
-
-    $('html').removeClass('no-js');
-
-    $body.on('click', '.btn-menu', function (e) {
-        $body.toggleClass('menu-open');
-    });
-
-    $body.on('click', '.close-nav', function (e) {
-        $body.removeClass('menu-open');
-    });
-
-    document.addEventListener('scroll', e => {
-        var wTop = document.documentElement.scrollTop || document.body.scrollTop;
-
-        //up
-        if (wTop <= 0 || wTop < oldScroll) {
-            $body.removeClass('hideHeader');
-        } else {
-            $body.addClass('hideHeader');
-        }
-
-        oldScroll = wTop;
-    }, { passive: true });
+    
+  
+        
+      
+      
+      });
 }.call(window, window.jQuery);
